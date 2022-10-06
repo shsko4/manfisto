4c4533553f533a2bbaf45ca7b8d07283d16a2c27
unknown <shsko4@gmail.com>
2022-10-06 18:05:08 +0200

    4

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     server: {
-        host: '192.168.222.3'
+        host: 'localhost'
       },
     plugins: [
         laravel({
